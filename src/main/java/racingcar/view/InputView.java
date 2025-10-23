b4e97f66e3a1a1d163c29bb39a219d1275863995
Chaeruin <cms3604@naver.com>
2025-10-24 08:16:38 +0900

    feat: 입력 뷰 클래스 내부 정적 중첩 검증 클래스로 자동차 이름들 입력 검증 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -6,11 +6,25 @@ public class InputView {
 
     public String getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+        return Validator.validateCarNames(Console.readLine());
     }
 
     public String getNumber(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         return Console.readLine();
+    }
+
+    public static class Validator {
+        // 자동차 이름 입력 검증
+        public static String validateCarNames(String message) {
+            return validateBlank(message);
+        }
+
+        private static String validateBlank(String message) {
+            if (message.isBlank()) {
+                throw new IllegalArgumentException();
+            }
+            return message;
+        }
     }
 }
