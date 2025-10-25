50b4130cca8f6076c8ef788537d3fac63ec32cfd
Chaeruin <cms3604@naver.com>
2025-10-25 14:58:39 +0900

    feat: GameController 구현 및 RacingGame 내부 불필요 메서드 제거, 타 메서드 정적 메서드로 변경

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,6 +1,10 @@
 package racingcar.controller;
 
+import java.util.List;
+import racingcar.domain.Car;
+import racingcar.service.Judgment;
 import racingcar.service.RacingGame;
+import racingcar.util.InputParser;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
@@ -8,11 +12,21 @@ public class GameController {
 
     private final InputView inputView;
     private final OutputView outputView;
-    private final RacingGame racingGame;
 
-    GameController(InputView inputView, OutputView outputView, RacingGame gameConfig) {
+    GameController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
-        this.racingGame = gameConfig;
+    }
+
+    public void run() {
+        List<Car> cars = RacingGame.createCars(InputParser.parseCarNames(inputView.getCarNames()));
+        int number = inputView.getNumber();
+        // outputView.printStart();
+        for (int i = 0; i < number; i++) {
+            RacingGame.executeOnce(cars);
+            // outputView.printExecutionOnce(cars);
+        }
+        List<String> winners = Judgment.getWinnersName(Judgment.getWinners(cars));
+        // outputView.printWinner(winners);
     }
 }
--- src/main/java/racingcar/service/RacingGame.java
+++ src/main/java/racingcar/service/RacingGame.java
@@ -5,20 +5,13 @@ import racingcar.domain.Car;
 
 public class RacingGame {
 
-    public List<Car> createCars(List<String> carNames) {
+    public static List<Car> createCars(List<String> carNames) {
         return carNames.stream()
                 .map(Car::new)
                 .toList();
     }
 
-    public List<Car> executeAll(List<Car> cars, int number) {
-        for (int i = 0; i < number; i++) {
-            cars = executeOnce(cars);
-        }
-        return cars;
-    }
-
-    private List<Car> executeOnce(List<Car> cars) {
+    public static List<Car> executeOnce(List<Car> cars) {
         return cars.stream()
                 .map(Car::moveForward)
                 .toList();
