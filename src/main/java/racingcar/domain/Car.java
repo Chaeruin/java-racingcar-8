78bdf70a90b968b143a318eab8c03f85e784d04a
Chaeruin <cms3604@naver.com>
2025-10-25 13:38:01 +0900

    feat: Car 도메인 구현 및 docs 업데이트

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void moveForward() {
+        if (isFourOrMore()) {
+            position++;
+        }
+    }
+
+    public boolean isFourOrMore() {
+        return Randoms.pickNumberInRange(0, 9) >= 4;
+    }
+}
