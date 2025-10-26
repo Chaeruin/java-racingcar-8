67fa2d3f6b7c218af7bdc30cd9022edad794c2d4
Chaeruin <cms3604@naver.com>
2025-10-26 14:37:18 +0900

    refactor: controller 반환값 재할당으로 변경

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -33,8 +33,8 @@ public class GameController {
 
     private void executeRacing(int number, List<Car> cars) {
         for (int i = 0; i < number; i++) {
-            RacingGame.executeOnce(cars);
-             outputView.printExecutionOnce(cars);
+            cars = RacingGame.executeOnce(cars);
+            outputView.printExecutionOnce(cars);
         }
     }
 }
