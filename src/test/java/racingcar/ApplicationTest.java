74ee1706cbb7d4602800390fd961e5bfb26c5349
Chaeruin <cms3604@naver.com>
2025-10-26 16:05:53 +0900

    test: ApplicationTest 통합테스트 작성

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -30,6 +31,79 @@ class ApplicationTest extends NsTest {
                 .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    @DisplayName("자동차 이름 입력 예외_빈 값")
+    void test1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "2"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수 예외_범위 예외")
+    void test2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun", "9000000000"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수 예외_범위 예외(양수 아님)")
+    void test3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun", "0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("시도 횟수 예외_문자 입력")
+    void test4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun", "abc"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름 파싱 예외_빈 이름1")
+    void test5() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,,jun", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름 파싱 예외_빈 이름2")
+    void test6() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",pobi,jun", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름 파싱 예외_이름 길이 5자 초과")
+    void test7() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun,abcdef", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("자동차 이름 파싱 예외_파싱 실패(구분자 오류)")
+    void test8() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("pobi,jun.abcd", "5"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 
     @Override
     public void runMain() {
